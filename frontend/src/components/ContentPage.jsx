f8422413dd17f94dc5f8151061fb8505c44666b1
fredrikkri <fjkri@hotmail.com>
2024-04-04 09:59:50 +0200

    gg

--- frontend/src/components/ContentPage.jsx
+++ frontend/src/components/ContentPage.jsx
@@ -2,12 +2,25 @@ import Title from './Title'
 import ProductCard from './ProductCard'
 import {products} from '../assets/legoduds'
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { fetchCategoryBySlug } from '../../sanity/services/categoryServices'
 export default function ContentPage({amount, setAmount, category, setCart, cart}){
-  const {id} = useParams()
+  const {slug} = useParams()
+  const [catInfo, setCatInfo] = useState(null)
+
+  const getCategoriesBySlug = async (slug) => {
+    const data = await fetchCategoryBySlug(slug)
+    setCatInfo(data)
+  } 
+
+useEffect(() => {
+  getCategoriesBySlug(slug)
+}, [slug])
+
     return(
         <main>
-          <Title category={id} />
-          {products.map(product => 
+          <Title category={catInfo?.categorytitle} />
+          {/* {products.map(product => 
           <ProductCard
           cart={cart} 
           setCart={setCart}
@@ -19,7 +32,7 @@ export default function ContentPage({amount, setAmount, category, setCart, cart}
           price={product.price}
           amount={amount}
           setAmount={setAmount}
-          /> )}
+          /> )} */}
         </main>
     )
 }
